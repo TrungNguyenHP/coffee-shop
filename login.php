d714b4676af97099fd29328eb3cbeea14e948059
TrungNguyenHP <152580377+TrungNguyenHP@users.noreply.github.com>
2024-08-26 16:44:52 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, 'coffee-shop.mysql.database.azure.com', 'tuan', 'Tohru14617', 'quickstartdb', 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, 'coffee-shop.mysql.database.azure.com', 'tuan', 'Tohru14617', 'coffee-shop', 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }

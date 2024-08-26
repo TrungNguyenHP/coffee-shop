c9e89aae18e1abf6a2ad3e06c6c1b1d12cb9857a
TrungNguyenHP <152580377+TrungNguyenHP@users.noreply.github.com>
2024-08-26 15:25:05 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,18 @@
 <?php
 session_start();
 
-require("D:\home\site\wwwroot\datacon.php");
+$servername = "coffee-shop.mysql.database.azure.com";
+$username = "tuan";
+$password = "Tohru14617";
+$database = "coffee-shop";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Include database connection
